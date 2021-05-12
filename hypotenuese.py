49b13ce7f656399f57715e0af59197c6faeec52c
Patel27-S <smit4632@gmail.com>
2021-05-12 12:56:14 -0400

    New file is added

---
+++ hypotenuese.py
@@ -0,0 +1,8 @@
+'''Program to print Hypotenuese'''
+
+def hypotenuese(a,b):
+    '''Function computes Hypotenuese'''
+    from math import sqrt as sqr    
+    c = sqr(a*a + b*b)
+    print(c)
+    
