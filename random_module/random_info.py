1cf411d17e9b6f1d71c76de6c16f7e71bddeaf6d
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2021-06-04 21:59:05 -0400

    Update random_info.py

--- random_module/random_info.py
+++ random_module/random_info.py
@@ -1,1 +1,41 @@
+import random
 
+'''
+In this particular application of random module, we will 
+use it to generate bogus data/info of some number of people.
+'random' as a module has functions such as 'uniform()',
+'randint()', 'choice()', 'choices()', 'sample()'.
+'''
+
+first_name = ['John','Kaman','Hardikbhai', 'Mark', 'Yuvi', 'Ricky', 'Brett', 'Colin', 'Mihirbhai', 'Digantbhai', 'Sahilbhai']
+
+last_name = ['Patel', 'Bush', 'Lee', 'Ponting', 'Johnson', 'Goradia', 'Warne', 'Zhu']
+
+street_names = ['Brooke Park Drive', 'Bellwood Rd', 'Canterburry Drive', 'Sherwood Circle', 'Main St.', 'Shreeji Rd', \
+    'Purshottam Drive', 'State Park Rd']
+
+city_name = ['Canton', 'Romulus', 'Livonia', 'Plymouth', 'Novi', 'Dearborn', 'Ann Arbor', 'Westland']
+
+state_name = ['NJ', 'IL', 'MI', 'CA', 'AZ', 'PA', 'OH', 'IN', 'NC', 'SC']
+
+
+
+for i in range(10):
+
+    f_name = random.choice(first_name)
+    print(f_name, end = ' ')
+
+    l_name = random.choice(last_name)
+    print(l_name+',')
+
+    print(random.randint(4562,8965), end = ' ')
+
+    print(random.choice(street_names)+',')
+
+    print(random.choice(city_name), end = ' ')
+
+    print(random.choice(state_name), end = '-')
+
+    print(random.randint(41632, 95632))
+
+    print()
