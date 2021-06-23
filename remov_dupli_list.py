d7a2c6d3dc4b41b11cfbe2aca9b994d93c270fae
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2021-06-23 15:53:45 -0400

    Delete remov_dupli_list.py

--- remov_dupli_list.py
+++
@@ -1,7 +0,0 @@
-'''
-The function below removes the duplicate elements from a 
-list and returns a list which has unique value of elements.
-'''
-
-def remove_duplicates(list1):
-  return list(set(list1))
