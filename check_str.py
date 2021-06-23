c07cf092132698107a728a7a84a7b13312fd0a1c
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2021-06-23 15:52:53 -0400

    Delete check_str.py

--- check_str.py
+++
@@ -1,9 +0,0 @@
-'''
-If the given string is already in lower or upper case
-the following function returns True or False otherwise.
-'''
-
-def check_low_up(string):
-  return string == string.lower() or string == string.upper()
-    
-
