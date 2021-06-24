0f834f645f7ad366c0470fbb8d772c8152bab441
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2021-06-24 10:00:37 -0400

    Delete hypotenuese.py

--- hypotenuese.py
+++
@@ -1,8 +0,0 @@
-'''Program to print Hypotenuese'''
-
-def hypotenuese(a,b):
-    '''Function computes Hypotenuese'''
-    from math import sqrt as sqr    
-    c = sqr(a*a + b*b)
-    print(c)
-    
